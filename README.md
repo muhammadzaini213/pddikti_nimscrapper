8dd9872b2b630d108c94b6fbaf5b35048602ff38
ibnucoding2423 <oniichanterritory@gmail.com>
2024-12-18 10:53:29 +0800

    main

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 1. Pastikan anda sudah memiliki python terinstall di perangkat anda
 2. Jalankan script berikut di terminal
 '''pip install pddikti.py'''
-3. Clone
+3. Clone Repository ini
+'''
--- scrap.py
+++ scrap.py
@@ -3,36 +3,36 @@ from pddiktipy import api
 a = api()
 
 nama_kampus = "INSTITUT TEKNOLOGI KALIMANTAN" # Ubah sesuai kampus yang ingin di scapping 
+limit = 99 # Limit berapa kali anda ingin melakukan scan
 
-for angkatan in range (18,24):
-    isFound = True
-    modified = 1 
-    output_file = f"data_if{angkatan}.txt" 
+isFound = True
+modified = 1 
+output_file = f"data.txt" 
 
-    with open(output_file, "w") as file:
-        file.write("Nama\tNIM\n")
+with open(output_file, "w") as file:
+    file.write("Nama\tNIM\n")
 
-        while isFound:
-            nim = f"11{angkatan}10{modified:02d}" # Pola NIM bisa disesuaikan
-            try:
-                result = a.search_all(nim)
-                if result and 'mahasiswa' in result and result['mahasiswa']:
-                    filtered_results = [
-                        student for student in result['mahasiswa']
-                        if student['nim'] == nim and student['nama_pt'] == nama_kampus
-                    ]
-                    if filtered_results:
-                        for student in filtered_results:
-                            file.write(f"{student['nama']}\t{student['nim']}\n")
-                        print(f"{student['nama']}\t{student['nim']}")
-                    else:
-                        if modified == 130:
-                            isFound = False  
+    while isFound:
+        nim = f"112410{modified:02d}" # Pola NIM bisa disesuaikan
+        try:
+            result = a.search_all(nim)
+            if result and 'mahasiswa' in result and result['mahasiswa']:
+                filtered_results = [
+                    student for student in result['mahasiswa']
+                    if student['nim'] == nim and student['nama_pt'] == nama_kampus
+                ]
+                if filtered_results:
+                    for student in filtered_results:
+                        file.write(f"{student['nama']}\t{student['nim']}\n")
+                    print(f"{student['nama']}\t{student['nim']}")
                 else:
-                    print("No students found or unexpected response.")
-                    isFound = False  
-            except Exception as e:
-                print(f"Error fetching data: {e}")
-                isFound = False  
-            
-            modified += 1
+                    if modified == limit:
+                        isFound = False  
+                    else:
+                        print("No students found or unexpected response.")
+                        isFound = False  
+        except Exception as e:
+            print(f"Error fetching data: {e}")
+            isFound = False  
+
+        modified += 1
